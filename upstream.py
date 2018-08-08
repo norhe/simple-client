5b92bb069303e1c215d9bdee6810028d4f157274
ehron <ehron@teamgwinn.com>
2018-08-08 17:33:42 -0400

    don't hardcode addresses

--- upstream.py
+++ upstream.py
@@ -1,10 +1,11 @@
-
+import os
 
 # At some point this could be dynamic.
 # But for now, use the local proxy
 def get_listing_addr():
-    return 'http://localhost:10002/listing'
+    listing_addr = os.environ.get('')
+    return "{0}/listing".format(os.getenv('LISTING_URI','http://localhost:10002/listing'))
 
 
 def get_product_addr():
-    return 'http://localhost:10001/product'
+    return "{0}/product".format(os.getenv('PRODUCT_URI', 'http://localhost:10001/product'))

20fb863b6213983cd908c04e19228d6b37ac66b7
Ehron Gwinn <ehron@local>
2018-11-27 22:00:37 -0500

    add install script

---
+++ install/install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# install the requirements
+sudo chown -R ubuntu:ubuntu /home/ubuntu/
+pip3 install flask
+pip3 install pymongo
+
+# download the apply
+mkdir /home/ubuntu/src
+cd /home/ubuntu/src
+git clone https://github.com/norhe/simple-client.git
+
+# systemd
+
+cat <<EOF | sudo tee /lib/systemd/system/web_client.service
+[Unit]
+Description=client.py - Client service API
+After=network.target
+
+[Service]
+Type=simple
+User=ubuntu
+ExecStart=/usr/local/bin/envconsul -prefix web_client_conf /usr/bin/python3 /home/ubuntu/src/simple-client/client.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable web_client.service
